3a4d94d9393e34c9482dd148ef5d7efdc2794ed1
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2022-03-30 10:54:02 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-var cursors = {}
+var cursors = {
+  "shrek": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/shrek.ico"
+};
+
+let selection = prompt("Which cursor would you want?");
+
+selection = selection.toLowerCase();
+
+selection = selection.replace(/\s/g, '');
+
+var css=`\n* {\n\tcursor: url('${cursors[selection]}'), auto;\n}`;
+style=document.createElement("style");
+style.innerHTML=css;
+document.head.appendChild(style);
