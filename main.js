919ab787d262516d58b0fe97ad063b0bfbacb72b
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2022-03-30 16:00:01 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ var cursors = {
   "cinamonroll": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/cinamonroll.jpg"
 };
 
+var elements = document.getElementsByTagName("*");
+
 let selection = prompt("Which cursor would you want?");
 
 selection = selection.toLowerCase();
@@ -21,16 +23,18 @@ try {
   console.log("Unable to play audio.")
 }
 
-document.registerEventHandler("click", () => {
-  try {
-    var audio=document.createElement("audio");
-    audio.src=`https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/${selection}.mp3`;
-    audio.loop=false;
-    audio.play()
-  } catch(a) {
-    console.log("Unable to play audio.")
-  }
-})
+for (var i = 0; i < elements.length; i++) {
+  elements[i].registerEventHandler("click", () => {
+    try {
+      var audio=document.createElement("audio");
+      audio.src=`https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/${selection}.mp3`;
+      audio.loop=false;
+      audio.play()
+    } catch(a) {
+      console.log("Unable to play audio.")
+    }
+  })
+}
 
 var css=`\n* {\n\tcursor: url('${cursors[selection]}'), auto;\n}`;
 style=document.createElement("style");
