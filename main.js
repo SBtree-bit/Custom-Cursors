4fc41afb064a3d0964e998c88180836a40ca3243
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2022-03-30 13:48:11 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 var cursors = {
-  "shrek_1": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/shrek_1.ico",
-  "kitty_1": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/kitty_1.jpeg",
-  "puppy_1": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/puppy_1.jpeg",
-  "amongus_1": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/amongus_1.png"
+  "shrek": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/shrek.ico",
+  "kitty": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/kitty.jpeg",
+  "puppy": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/puppy.jpeg",
+  "amongus": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/amongus.png"
 };
 
 let selection = prompt("Which cursor would you want?");
 
 selection = selection.toLowerCase();
+
+selection = selection.replace(/\s/g, '');
 
 var css=`\n* {\n\tcursor: url('${cursors[selection]}'), auto;\n}`;
 style=document.createElement("style");
