a9f5733e97197c44aa38f12cab8127662ac50b5e
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2022-03-30 21:07:02 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,52 @@
-var cursors = {
-  "shrek": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/shrek.ico",
-  "kitty": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/kitty.jpeg",
-  "puppy": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/puppy.jpeg",
-  "amongus": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/amongus.png",
-  "cinnamoroll": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/cinamoroll.jpg"
-};
+function run() {
+  var cursors = {
+    "shrek": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/shrek.ico",
+    "kitty": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/kitty.jpeg",
+    "puppy": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/puppy.jpeg",
+    "amongus": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/amongus.png",
+    "cinnamoroll": "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/cinamoroll.jpg"
+  };
 
-var elements = document.getElementsByTagName("*");
+  var elements = document.getElementsByTagName("*");
 
-let selection = prompt("Which cursor would you want?");
+  let selection = prompt("Which cursor would you want?");
 
-selection = selection.toLowerCase();
+  selection = selection.toLowerCase();
 
-selection = selection.replace(/\s/g, '');
+  selection = selection.replace(/\s/g, '');
 
-try {
-  var audio=document.createElement("audio");
-  audio.src=`https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/${selection}.mp3`;
-  audio.loop=false;
-  audio.play()
-} catch(a) {
-  console.log("Unable to play audio.")
-}
-
-for (var i = 0; i < elements.length; i++) {
-  var listener = function handleClick(event){
-    try {
-      var audio=document.createElement("audio");
-      audio.src=`https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/${selection}.mp3`;
-      audio.loop=false;
-      audio.play()
-    } catch(a) {
-      console.log("Unable to play audio.")
-    } 
+  try {
+    var audio=document.createElement("audio");
+    audio.src=`https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/${selection}.mp3`;
+    audio.loop=false;
+    audio.play()
+  } catch(a) {
+    console.log("Unable to play audio.")
   }
-  elements[i].removeEventListener("click", listener);
-  elements[i].addEventListener('click', listener);
+
+  for (var i = 0; i < elements.length; i++) {
+    var listener = function handleClick(event){
+      try {
+        var audio=document.createElement("audio");
+        audio.src=`https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/${selection}.mp3`;
+        audio.loop=false;
+        audio.play()
+      } catch(a) {
+        console.log("Unable to play audio.")
+      } 
+    }
+    elements[i].removeEventListener("click", listener);
+    elements[i].addEventListener('click', listener);
+  }
+
+  var css=`\n* {\n\tcursor: url('${cursors[selection]}'), auto;\n}`;
+  style=document.createElement("style");
+  style.innerHTML=css;
+  document.head.appendChild(style);
 }
 
-var css=`\n* {\n\tcursor: url('${cursors[selection]}'), auto;\n}`;
-style=document.createElement("style");
-style.innerHTML=css;
-document.head.appendChild(style);
+run();
+
+window.frame.forEach(() => {
+  run();
+})
