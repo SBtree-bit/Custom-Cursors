d32f5d929ddd1028bf5687cb4e4d1dab0ada49b2
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2022-04-03 21:06:11 -0700

    Update client.js

--- amongus/client.js
+++ amongus/client.js
@@ -1,7 +1,8 @@
 var tasks = [
   "https://raw.githubusercontent.com/SBtree-bit/Custom-Cursors/main/amongus/tasks/wires.js"
 ]
-var randomTask = tasks[(Math.random()*26)]
+//var randomTask = tasks[(Math.random()*25)]
+var randomTask = tasks[0]
 if (!taskreq) {
   var taskreq = new XMLHttpRequest()
 }
